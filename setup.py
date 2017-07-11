2d6a222d7565189ef1f0ab89ff56f2caecc79084
Pedro Regis <pedroalencarregis@hotmail.com>
2017-07-10 21:44:13 -0300

    Add ths link to the github repository

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mwePyPI',
       version='0.0.1',
       description='It is a minimal work exemple of a package to be uploaded in PyPI.',
-      url='I am going to crate a github repo to this',
+      url='https://github.com/PedroRegisPOAR/mwePyPi',
       author='Pedro Osvaldo Alencar Regis',
       author_email='pedroalencarregis@hotmail.com',
       license='MIT',
