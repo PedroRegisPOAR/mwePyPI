15f14e0059102a67154bc5324aa5e6f816fe7bc9
pedroregispoar <pedroalencarregis@hotmail.com>
2018-06-13 22:20:04 +0000

    Update version number 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mwePyPI',
-      version='0.0.1',
+      version='0.0.2',
       description='It is a minimal work exemple of a package to be uploaded in PyPI.',
       url='https://github.com/PedroRegisPOAR/mwePyPi',
       author='Pedro Osvaldo Alencar Regis',
